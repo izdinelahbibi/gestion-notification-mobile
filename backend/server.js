24758e353765972fd1c6f0f2177dc1f170ffa2c6
Yousef Hbibi <youssefhbibi43gmail.com>
2024-10-30 15:09:15 +0100

    add

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',     // Remplacez par votre nom d'utilisateur MySQL
-  password: 'root', // Remplacez par votre mot de passe MySQL
+  password: '', // Remplacez par votre mot de passe MySQL
   database: 'user_database'
 });
 
